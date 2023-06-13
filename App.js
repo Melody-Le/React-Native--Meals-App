7e53a5cf666a82eceaf49605034ca4c464cacc31
muile <tanmui1992@gmail.com>
2023-06-14 00:34:20 +0800

    feat: Install navigation, and add Categories into the first navigation

--- App.js
+++ App.js
@@ -2,12 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CategoryGridTile from './components/CategoryGridTile';
 import CategoriesScreen from './screens/CategoriesScreen';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <>
-      <StatusBar style='light' />
-      <CategoriesScreen />
+      <StatusBar style='dark' />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='MealsCategories' component={CategoriesScreen} />
+        </Stack.Navigator>
+        {/* <CategoriesScreen /> */}
+      </NavigationContainer>
     </>
   );
 }

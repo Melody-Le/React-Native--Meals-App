598ec960b6850a866f0ca6ae79dda0f37443b7e0
muile <tanmui1992@gmail.com>
2023-06-14 19:24:03 +0800

    feat: transfer data between 2 component by useing route props

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import CategoryGridTile from './components/CategoryGridTile';
 import CategoriesScreen from './screens/CategoriesScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+import MealsOverviewScreen from './screens/MealsOverviewScreen';
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (

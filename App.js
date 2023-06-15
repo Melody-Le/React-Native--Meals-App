01742c8ea74c6ffe22f094e202daf7ec1cf1ecbc
muile <tanmui1992@gmail.com>
2023-06-15 23:28:22 +0800

    feat: settup color for screen

--- App.js
+++ App.js
@@ -5,14 +5,29 @@ import CategoriesScreen from './screens/CategoriesScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <>
       <StatusBar style='dark' />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name='MealsCategories' component={CategoriesScreen} />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: '#351401' },
+            headerTintColor: 'white',
+            contentStyle: {
+              backgroundColor: '#48a0b6',
+            },
+          }}
+        >
+          <Stack.Screen
+            name='MealsCategories'
+            component={CategoriesScreen}
+            options={{
+              title: 'Meal Categories',
+            }}
+          />
           <Stack.Screen name='MealsOverview' component={MealsOverviewScreen} />
         </Stack.Navigator>
       </NavigationContainer>
@@ -23,7 +38,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#957c7c',
     alignItems: 'center',
     justifyContent: 'center',
   },
